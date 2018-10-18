8306891ee205ace379f25e9e8a9acea06533bdb5
Riot361 <44266627+Riot361@users.noreply.github.com>
2018-10-18 15:39:18 +0100

    upload project

---
+++ A-Level_Project_Racing/A-Level_Project_Racing/Game1.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+using Squared.Tiled;
+using System.Collections.Generic;
+
+namespace A_Level_Project_Racing
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Map map;
+		ObjectGroup collision;
+		Vector2 viewportPosition;
+		int tilepixel;
+		Texture2D cartexture;
+		PlayerCar car;
+		Texture2D marker;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			graphics.PreferredBackBufferWidth = 1280;
+			graphics.PreferredBackBufferHeight = 720;
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+			graphics.PreferredBackBufferWidth = 1280;
+			graphics.PreferredBackBufferHeight = 640;
+			graphics.ApplyChanges();
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			map = Map.Load(Path.Combine(Content.RootDirectory, "Track 1.tmx"), Content);
+			collision = map.ObjectGroups["Object Layer 1"];
+			tilepixel = map.TileWidth;
+			
+			cartexture = Content.Load<Texture2D>("PlayerCar");
+			map.ObjectGroups["Object Layer 1"].Objects["PLAYER"].Texture = cartexture;
+			Vector2 carPos = new Vector2((graphics.PreferredBackBufferWidth / 2) - (cartexture.Width / 2),
+				(graphics.PreferredBackBufferHeight / 2) - (cartexture.Height / 2));
+			car = new PlayerCar();
+			car.Init(cartexture, carPos);
+			car.Position = new Vector2(map.ObjectGroups["Object Layer 1"].Objects["PLAYER"].X - 640 , map.ObjectGroups["Object Layer 1"].Objects["PLAYER"].Y-320);
+// TODO: use this.Content to load your game content here
+		}
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+			car.Update(gameTime);
+			map.ObjectGroups["Object Layer 1"].Objects["PLAYER"].X = (int)car.Position.X;
+			map.ObjectGroups["Object Layer 1"].Objects["PLAYER"].Y = (int)car.Position.Y;
+
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+			spriteBatch.Begin();
+			map.Draw(spriteBatch, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), car.Position);
+
+			car.Draw(spriteBatch);
+			spriteBatch.End();
+
+			// TODO: Add your drawing code here
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ A-Level_Project_Racing/A-Level_Project_Racing/PlayerCar.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+
+namespace A_Level_Project_Racing
+{
+	class PlayerCar
+	{
+		Texture2D texture;
+		Vector2 position;
+		Vector2 origin;
+		float steer = 0;
+		float speed = 0;
+		float slide = 0;
+		float tempsteer = 0;
+		public void Init(Texture2D t, Vector2 pos)
+		{
+			position = pos;
+			texture = t;
+			origin = new Vector2(t.Width / 2, t.Height / 2);
+		}
+		public void GetInputs(KeyboardState input)
+		{
+			if (speed != 0)
+			{
+				if (input.IsKeyDown(Keys.Left))
+				{
+					steer -= (.05f);
+				}
+
+				if (input.IsKeyDown(Keys.Right))
+				{
+					steer += (.05f);
+				}
+
+			}
+
+			
+			if (input.IsKeyDown(Keys.Up) && speed < 15)
+			{
+				speed += .5f;
+			}
+
+			if (input.IsKeyDown(Keys.Down) && speed > -5)
+			{
+				speed -= .25f;
+			}
+			if (speed*speed > 200 && (input.IsKeyDown(Keys.Left) || input.IsKeyDown(Keys.Right)))
+			{
+
+				float steerdif = steer - tempsteer;
+				tempsteer = steer;
+				slide = speed * steerdif;
+			}
+			else
+			{
+				slide = 0;
+			}
+
+			if (speed > 0)
+				speed -= .25f;
+			else if (speed < 0)
+				speed += .125f;
+			else
+				speed = 0;
+		}
+		public void Update(GameTime gameTime)
+		{
+			GetInputs(Keyboard.GetState());
+			position = position + new Vector2(speed * (float)Math.Cos(steer), speed * (float)Math.Sin(steer));
+
+		}
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(texture, origin+new Vector2(640,320), null, null, origin, steer, null, null, SpriteEffects.FlipHorizontally, 0);
+		}
+		public Vector2 Position
+		{
+			get { return position; }
+			set { position = value; }
+
+		}
+
+	}
+}
